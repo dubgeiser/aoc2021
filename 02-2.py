0a69f7e1538cc449e9321ed0a583c7b74db62f0c
Per Juchtmans <dubgeiser@gmail.com>
2021-12-03 14:27:20 +0100

    Day 2, part 2
    First First Try :-)

---
+++ 02-2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# AOC2021 - Day 2, part 2
+
+def forward(d, pos):
+    x, y, aim = pos
+    x += d
+    y += aim * d
+    return (x, y, aim)
+
+def down(d, pos):
+    x, y, aim = pos
+    aim += d
+    return (x, y, aim)
+
+def up(d, pos):
+    x, y, aim = pos
+    aim -= d
+    return (x, y, aim)
+
+def answer(pos):
+    return pos[0] * pos[1]
+
+
+def test_movement():
+    pos = (0, 0, 0)
+
+    pos = forward(5, pos)
+    assert pos == (5, 0, 0)
+
+    pos = down(5, pos)
+    assert pos == (5, 0, 5)
+
+    pos = forward(8, pos)
+    assert pos == (13, 40, 5)
+
+    pos = up(3, pos)
+    assert pos == (13, 40, 2)
+
+    pos = down(8, pos)
+    assert pos == (13, 40, 10)
+
+    pos = forward(2, pos)
+    assert pos == (15, 60, 10)
+
+    assert answer(pos) == 900
+
+
+test_movement()
+
+fn = "02.dat"
+pos = (0, 0, 0)
+with open(fn) as data:
+    for movement in data:
+        op, delta = movement.split()
+        delta = int(delta)
+        op = globals()[op]
+        pos = op(delta, pos)
+
+# Dumbass submitted answer from example data too many times already.
+prefix = ""
+if "example" in fn:
+    prefix = "EXAMPLE DATA!!!! "
+
+print(f"{prefix}{answer(pos)= }")
