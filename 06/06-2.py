868ae9bc0a1efb9d0daa816e8b438ca9c1b9fdb6
Per Juchtmans <dubgeiser@gmail.com>
2021-12-06 18:04:52 +0100

    Day 6, part 2

---
+++ 06/06-2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+def read_data_file(fn):
+    return [int(i) for i in open(fn).readline().strip().split(",")]
+
+
+fn = "06.dat"
+data = read_data_file(fn)
+total = len(data)
+start = 1
+end = 256
+
+# fishcounter by timer
+# index = timer
+# value = number of fish
+timers = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in data:
+    timers[i] += 1
+
+while start <= end:
+    no_fish_delivering = timers[0]
+    timers[0] = timers[1]
+    timers[1] = timers[2]
+    timers[2] = timers[3]
+    timers[3] = timers[4]
+    timers[4] = timers[5]
+    timers[5] = timers[6]
+    timers[6] = timers[7] + no_fish_delivering
+    timers[7] = timers[8]
+    timers[8] = no_fish_delivering
+    start += 1
+
+print(sum(timers))
