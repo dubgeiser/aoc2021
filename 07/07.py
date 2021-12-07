99eef6ecb0b6ad28e6c93dc7b86475f5158f5e17
Per Juchtmans <dubgeiser@gmail.com>
2021-12-07 17:56:37 +0100

    Day 7, part 1

---
+++ 07/07.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def read_data_file(fn):
+    return [int(i) for i in open(fn).readline().strip().split(",")]
+
+
+fn = "07.dat"
+positions = read_data_file(fn)
+pos_min = min(positions)
+pos_max = max(positions) + 1
+possible_positions = [i for i in range(pos_min, pos_max)]
+fuel_usage = []
+fuel_max_usage = pos_max * len(positions)
+
+for pp in possible_positions:
+    fuel = 0
+    for p in positions:
+        fuel += abs(p - pp)
+    if fuel < fuel_max_usage:
+        fuel_usage.append(fuel)
+        fuel_max_usage = fuel
+
+print(min(fuel_usage))
